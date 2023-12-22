c3456d6f772167d1f66afd81390aa809f11a043b
romli <mohammadromli098@gmail.com>
2023-12-23 00:59:53 +0700

    Aplikasi catatan v3

---
+++ src/components/Network.js
@@ -0,0 +1,160 @@
+import React, { } from "react";
+  
+import {
+    Navigate ,
+    useLocation
+  } from "react-router-dom";
+export const setToken = (token) =>{
+    // set token in localStorage
+    localStorage.setItem('Token', token)
+}
+export const fetchToken = (token) =>{
+    // fetch the token
+    return localStorage.getItem('Token')
+}
+export function RequireToken({children}) {
+      
+    let auth = fetchToken()
+    let location = useLocation();
+    
+    if (!auth) {
+        
+      return <Navigate to="/login" state={{ from: location }} />;
+    }
+    
+    return children;
+}
+const BASE_URL = "http://165.22.55.164:3000/api";
+
+function getAccessToken() {
+  return localStorage.getItem("accessToken");
+}
+
+function putAccessToken(accessToken) {
+  return localStorage.setItem("accessToken", accessToken);
+}
+
+async function fetchWithToken(url, options = {}) {
+  return fetch(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
+}
+
+async function login({ username, password }) {
+  const response = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  const responseJson = await response.json();
+  console.log(responseJson);
+  if (response.status >= 400) {
+    alert(responseJson.msg);
+    return { error: true, code: response.status, data: null };
+  }
+
+  return { error: false, code: response.status, data: responseJson.data };
+}
+
+async function register({ user, password }) {
+  const response = await fetch(`${BASE_URL}/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ user, password }),
+  });
+
+  const responseJson = await response.json();
+
+  if (response.status >= 400) {
+    alert(responseJson.msg);
+    return { error: true, code: response.status };
+  }
+
+  return { error: false, code: response.status };
+}
+
+async function getUserLogged() {
+  const response = await fetchWithToken(`${BASE_URL}/users/own-profile`);
+  const responseJson = await response.json();
+
+  if (response.status >= 400) {
+    return { error: true, code: response.status, data: null };
+  }
+
+  return { error: false, code: response.status, data: responseJson.data };
+}
+
+async function addNote({ title, body }) {
+  const response = await fetchWithToken(`${BASE_URL}/notes`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, body }),
+  });
+
+  const responseJson = await response.json();
+
+  if (response.status >= 400) {
+    return { error: true, code: response.status, data: null };
+  }
+
+  return { error: false, code: response.status, data: responseJson.data };
+}
+
+async function getNotes() {
+  const response = await fetchWithToken(`${BASE_URL}/notes`);
+  const responseJson = await response.json();
+
+  if (response.status >= 400) {
+    return { error: true, code: response.status, data: null };
+  }
+
+  return { error: false, code: response.status, data: responseJson.data };
+}
+
+async function getNote(id) {
+  const response = await fetchWithToken(`${BASE_URL}/notes/${id}`);
+  const responseJson = await response.json();
+
+  if (response.status >= 400) {
+    return { error: true, code: response.status, data: null };
+  }
+
+  return { error: false, code: response.status, data: responseJson.data };
+}
+
+async function deleteNote(id) {
+  const response = await fetchWithToken(`${BASE_URL}/notes/${id}`, {
+    method: "DELETE",
+  });
+
+  const responseJson = await response.json();
+
+  if (response.status >= 400) {
+    return { error: true, code: response.status, data: null };
+  }
+
+  return { error: false, code: response.status, data: responseJson.data };
+}
+
+export {
+  getAccessToken,
+  putAccessToken,
+  login,
+  register,
+  getUserLogged,
+  addNote,
+  getNotes,
+  getNote,
+  deleteNote,
+};
